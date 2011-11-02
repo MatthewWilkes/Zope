b9dab325343856180a920797e4290f7ebc9a2c4e
Mikko <mikko@mfabrik.com>
2011-11-02 21:38:54 +0200

    Explicitly support id traversing when subobject access is turned off

--- src/OFS/SimpleItem.py
+++ src/OFS/SimpleItem.py
@@ -90,8 +90,13 @@ class Item(Base,
         pass
     manage_afterClone.__five_method__ = True
 
+    # XXX: Remove exposed id attribute altogether
     # Direct use of the 'id' attribute is deprecated - use getId()
     id = ''
+
+    # Expose id attribute for security machinary
+    # explicitly 
+    security.declarePublic('id')
 
     security.declarePublic('getId')
     def getId(self):
@@ -128,7 +133,7 @@ class Item(Base,
     REQUEST = Acquired
 
     # Allow (reluctantly) access to unprotected attributes
-    __allow_access_to_unprotected_subobjects__=1
+    __allow_access_to_unprotected_subobjects__=0
 
     def title_or_id(self):
         """Return the title if it is not blank and the id otherwise.
